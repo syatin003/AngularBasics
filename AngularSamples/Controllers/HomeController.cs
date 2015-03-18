af9c1f79054902ae6eb3355593a559f9b11d5608
yatin sharma <sharmayatin489@gmail.com>
2015-03-18 18:05:07 +0530

    sample4

--- AngularSamples/Controllers/HomeController.cs
+++ AngularSamples/Controllers/HomeController.cs
@@ -88,5 +88,173 @@ namespace AngularSamples.Controllers
         {
             return View();
         }
+        public ActionResult fxn_bootstrap()
+        {
+            return View();
+        }
+        public ActionResult fxn_copy()
+        {
+            return View();
+        }
+        public ActionResult dir_form()
+        {
+            return View();
+        }
+        public ActionResult dir_inputCheckbox()
+        {
+            return View();
+        }
+        public ActionResult dir_inputDate()
+        {
+            return View();
+        }
+        public ActionResult dir_inputDateLocal()
+        {
+            return View();
+        }
+        public ActionResult dir_inputEmail()
+        {
+            return View();
+        }
+        public ActionResult dir_inputMonth()
+        {
+            return View();
+        }
+        public ActionResult dir_inputNumber()
+        {
+            return View();
+        }
+        public ActionResult dir_inputRadio()
+        {
+            return View();
+        }
+        public ActionResult dir_inputText()
+        {
+            return View();
+        }
+        public ActionResult dir_inputTime()
+        {
+            return View();
+        }
+        public ActionResult dir_inputUrl()
+        {
+            return View();
+        }
+        public ActionResult dir_inputWeek()
+        {
+            return View();
+        }
+        public ActionResult ng_app()
+        {
+            return View();
+        }
+        public ActionResult ng_bind_template()
+        {
+            return View();
+        }
+        public ActionResult ng_change()
+        {
+            return View();
+        }
+        public ActionResult ng_checked()
+        {
+            return View();
+        }
+        public ActionResult ng_class()
+        {
+            return View();
+        }
+        public ActionResult ng_even_odd()
+        {
+            return View();
+        }
+        public ActionResult ng_cloak()
+        {
+            return View();
+        }
+        public ActionResult ng_controller()
+        {
+            return View();
+        }
+        public ActionResult ng_copy_cut_paste_ReadOnly()
+        {
+            return View();
+        }
+        public ActionResult ng_dblClick()
+        {
+            return View();
+        }
+        public ActionResult ng_disabled()
+        {
+            return View();
+        }
+        public ActionResult ng_hideShow()
+        {
+            return View();
+        }
+        public ActionResult ng_href()
+        {
+            return View();
+        }
+        public ActionResult ng_if()
+        {
+            return View();
+        }
+        public ActionResult ng_include()
+        {
+            return View();
+        }
+        public ActionResult ng_keyDown_keyPress_keyUp()
+        {
+            return View();
+        }
+        public ActionResult ng_model()
+        {
+            return View();
+        }
+        public ActionResult ng_model_Option()
+        {
+            return View();
+        }
+        public ActionResult ng_mouseDown_Enter_Leave_Move_Over_()
+        {
+            return View();
+        }
+        public ActionResult ng_open()
+        {
+            return View();
+        }
+        public ActionResult ng_Options_selected()
+        {
+            return View();
+        }
+        public ActionResult ng_pluralize()
+        {
+            return View();
+        }
+        public ActionResult ng_repeat()//search
+        {
+            return View();
+        }
+        public ActionResult ng_style()
+        {
+            return View();
+        }
+        public ActionResult ng_submit()
+        {
+            return View();
+        }
+        public ActionResult ng_switch_when()
+        {
+            return View();
+        }
+        public ActionResult ng_transclude()
+        {
+            return View();
+        }
+        public ActionResult ng_value()
+        {
+            return View();
+        }
     }
 }
